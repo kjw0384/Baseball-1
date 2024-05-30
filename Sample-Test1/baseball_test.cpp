404d307081dacf4a00f49b7107bb3eb5d7aa166c
kjw0384 <kjw0384@gmail.com>
2024-05-30 15:27:01 +0900

    case4 green  - 정답 적중 시 case  - 결과 struct(GuessResult) 추가

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 class BaseballFixture : public testing::Test {
 public:
-	Baseball game;
+	Baseball game{ "123" };
+	//Baseball game;
 	void assertIllegalArgument(string guessNumber) {
 		//game.guess() 수행 후, Exception이 발생해야 pass 이다.
 		try {
@@ -23,4 +24,12 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
