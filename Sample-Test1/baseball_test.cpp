0e03e6a97463c689653b3b41e79831aee04455d1
kjw0384 <kjw0384@gmail.com>
2024-05-30 15:02:54 +0900

    case2 refactor  - test : fixture 추가, Exceptiopn test 케이스 통합  - baseball : 내 이름 rename

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -4,12 +4,21 @@
 
 using namespace std;
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
-}
+	void assertIllegalArgument(string guessNumber) {
+		//game.guess() 수행 후, Exception이 발생해야 pass 이다.
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			//PASS
+		}
+	}
+};
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12s");
 }
